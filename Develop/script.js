d5c53ff67666b1917af9e05da28ca5ab7ba3b3d0
Michael Ramos <michaelaramos182@gmail.com>
2022-04-03 18:54:04 -0500

    Added in text block areas and assigned css attributes, added in save button icons to save tasks.

--- Develop/script.js
+++ Develop/script.js
@@ -26,4 +26,37 @@ for (i = 9; i <= 17; i++) {
 
     row.append(timeBlock);
 
+    // set schedule text blocks
+
+    let textBlock = $('<textarea>');
+    textBlock.addClass('description col-8' + userHours[i-9]);
+
+    // assigning block colors based on time sets
+    if (i < userHours) {
+        textBlock.addClass('pass');
+    }
+
+    if (i === userHours) {
+        textBlock.addClass('present');
+    }
+
+    if (i > userHours) {
+        textBlock.addClass('future');
+    }
+
+    row.append(textBlock);
+
+
+    // setting save button to dom and appending content to page
+
+    let buttonBlock = $('<div>');
+    buttonBlock.addClass('saveBtn col');
+    buttonBlock.append("<i class=\"far fa-save " + userHours[i-9] + "\"></i>")
+    row.append(buttonBlock);
+
+    // pulling local storage and passing through array
+
+    let classIcon = textBlock.attr('class').split('') [2];
+    let tasks = localStorage.getItem(classIcon);
+    textBlock.text(tasks);
 }
