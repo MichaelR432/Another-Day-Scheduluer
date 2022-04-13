5f1ae39cfb7714a19e7b3fda444037411073ef4e
Michael Ramos <michaelaramos182@gmail.com>
2022-04-13 17:23:36 -0500

    Added in functionality to add and save tasks to DOM, fixed the moment bug that allowed time class colors to show.

--- Develop/script.js
+++ Develop/script.js
@@ -4,21 +4,21 @@ $('#currentDay').text(time);
 
 /// Setting current time of user
 
-let currentTime = moment().format("HH");
+let currentTime = parseInt(moment().format('H'));
 
 /// Setting up an array of user hours
-let userHours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+let userHours = ["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"]
 
 // For loop to set user time blocks
 
 for (i = 9; i <= 17; i++) {
 
-    let row = $('div');
-    row.addClass(" row time-block");
+    let row = $('<div>');
+    row.addClass("row time-block");
     $(".container").append(row);
 
-    let timeBlock = $('div');
-    timeBlock.addClass(" col hour ");
+    let timeBlock = $('<div>');
+    timeBlock.addClass("hour col");
 
     let hourText = $('<h4>');
     hourText.text(userHours[i - 9]);
@@ -29,18 +29,18 @@ for (i = 9; i <= 17; i++) {
     // set schedule text blocks
 
     let textBlock = $('<textarea>');
-    textBlock.addClass('description col-8' + userHours[i-9]);
+    textBlock.addClass('col-8 description ' + userHours[i - 9]);
 
     // assigning block colors based on time sets
-    if (i < userHours) {
-        textBlock.addClass('pass');
+    if (i < currentTime) {
+        textBlock.addClass('past');
     }
 
-    if (i === userHours) {
+    if (i === currentTime) {
         textBlock.addClass('present');
     }
 
-    if (i > userHours) {
+    if (i > currentTime) {
         textBlock.addClass('future');
     }
 
@@ -51,12 +51,22 @@ for (i = 9; i <= 17; i++) {
 
     let buttonBlock = $('<div>');
     buttonBlock.addClass('saveBtn col');
-    buttonBlock.append("<i class=\"far fa-save " + userHours[i-9] + "\"></i>")
+    buttonBlock.append("<i class=\"far fa-save " + userHours[i-9] + "\"></i>");
     row.append(buttonBlock);
 
     // pulling local storage and passing through array
 
-    let classIcon = textBlock.attr('class').split('') [2];
-    let tasks = localStorage.getItem(classIcon);
+    let saveIcon = textBlock.attr('class').split('')[2];
+    let tasks = localStorage.getItem(saveIcon);
     textBlock.text(tasks);
 }
+
+// sending tasks to local storage
+$(document).on('click', '.far', function() {
+
+    const saveIcon = $(this).attr('class').split('')[2];
+    const textArea = $('textarea' + saveIcon);
+    const tasksInput = textArea.val();
+
+    let tasks = localStorage.setItem(saveIcon, tasksInput);
+})
