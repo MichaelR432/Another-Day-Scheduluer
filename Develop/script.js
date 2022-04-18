44d39ac97f7b8ea04eba277b3a0e8f274a8ce054
Michael Ramos <michaelaramos182@gmail.com>
2022-04-18 17:59:02 -0500

    Fixed the bug in my code. Local storage wasn't saving because my time as string values had a space, removed space and application worked again.

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ $('#currentDay').text(time);
 let currentTime = parseInt(moment().format('H'));
 
 /// Setting up an array of user hours
-let userHours = ["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"]
+let userHours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 
 // For loop to set user time blocks
 
@@ -51,21 +51,21 @@ for (i = 9; i <= 17; i++) {
 
     let buttonBlock = $('<div>');
     buttonBlock.addClass('saveBtn col');
-    buttonBlock.append("<i class=\"far fa-save " + userHours[i-9] + "\"></i>");
+    buttonBlock.append("<i class=\"far fa-save " + userHours[i - 9] + "\"></i>");
     row.append(buttonBlock);
 
     // pulling local storage and passing through array
 
-    let saveIcon = textBlock.attr('class').split('')[2];
+    let saveIcon = textBlock.attr('class').split(' ')[2];
     let tasks = localStorage.getItem(saveIcon);
     textBlock.text(tasks);
 }
 
 // sending tasks to local storage
-$(document).on('click', '.far', function() {
+$(document).on("click", ".far", function() {
 
-    const saveIcon = $(this).attr('class').split('')[2];
-    const textArea = $('textarea' + saveIcon);
+    const saveIcon = $(this).attr('class').split(' ')[2];
+    const textArea = $('textarea.' + saveIcon);
     const tasksInput = textArea.val();
 
     let tasks = localStorage.setItem(saveIcon, tasksInput);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Another-Day-Scheduluer
+
+## User Story
+
+AS AN Student with a busy schedule
+I WANT to add important events to a daily 
+planner SO THAT I can manage my time 
+effectively
+When I open the daily planner it shows 
+the current time and day at the top and 
+blocks for each hour of the day
+So that I can click on a hour block and 
+enter in a task for the time
+When I view the time blocks for that day
+Then each time block is color-coded to 
+indicate whether it is in the past, 
+present, or future
+
+### Screen Shots
+![screenshot1](/images/SS.png)
+
+![screenshot1](/images/SS1.png)
